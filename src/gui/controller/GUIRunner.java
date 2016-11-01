7ed666812fa063a2ba7049d0125a6cb96a99d340
Pbra1484 <pbra1484@740s-pl-108.local>
2016-11-01 11:50:46 -0600

    Project started

---
+++ src/gui/controller/GUIRunner.java
@@ -0,0 +1,11 @@
+package gui.controller;
+
+public class GUIRunner
+{
+	public static void main (String [] args)
+	{
+		GUIController gUIAppController = new GUIController();
+		gUIAppController.start();
+	}
+
+}
