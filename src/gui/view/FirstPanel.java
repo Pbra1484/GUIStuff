f2961ca194b9a458aa770803bb512d8e34e1382c
Pbra1484 <pbra1484@740s-pl-108.local>
2016-11-01 13:30:35 -0600

    changes background to random color

--- src/gui/view/FirstPanel.java
+++ src/gui/view/FirstPanel.java
@@ -1,24 +1,28 @@
 package gui.view;
 
-import javax.swing.JPanel;
 import gui.controller.GUIController;
 import java.awt.Color;
-import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.*;
 
 public class FirstPanel extends JPanel
 {
 	private GUIController baseController;
 	private JButton colorButton;
 	private JButton randomButton;
+	private SpringLayout baseLayout;
 	
 	
 	public FirstPanel(GUIController baseController)
 	{
 		super();
 		this.baseController = baseController;
-		
-		this.colorButton = new JButton("colors");
+		this.baseLayout = new SpringLayout();	
+		this.colorButton = new JButton("colors");	
+	
 		this.randomButton = new JButton("random colors");
+		
 		
 		setupPanel();
 		setupLayout();
@@ -29,6 +33,7 @@ public class FirstPanel extends JPanel
 	
 	private void setupPanel()
 	{
+		this.setLayout(baseLayout);
 		this.setBackground(Color.BLACK);
 		this.add(colorButton);
 		this.add(randomButton);
@@ -37,11 +42,70 @@ public class FirstPanel extends JPanel
 	
 	private void setupLayout()
 	{
+		baseLayout.putConstraint(SpringLayout.WEST, colorButton, 98, SpringLayout.WEST, this);
+		baseLayout.putConstraint(SpringLayout.SOUTH, colorButton, -77, SpringLayout.SOUTH, this);	
+		baseLayout.putConstraint(SpringLayout.SOUTH, randomButton, -214, SpringLayout.SOUTH, this);
+		baseLayout.putConstraint(SpringLayout.EAST, randomButton, -98, SpringLayout.EAST, this);
+	
+		
+		
+		
+		
 		
 	}
 	
 	private void setupListeners()
 	{
+		colorButton.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent click)
+			{
+				setBackground(Color.ORANGE);
+			}
+		});
+		
+		randomButton.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent click)
+			{
+				changeBackgroundColor();
+			}
+		});
+		
+		
+		
 		
 	}
+	
+	
+	
+	
+	
+	
+	
+	private void changeBackgroundColor()
+	{
+		int red = (int) (Math.random() * 256);
+		int green = (int) (Math.random() * 256);
+		int blue = (int) (Math.random() * 256);
+		this.setBackground(new Color(red, green, blue));
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
