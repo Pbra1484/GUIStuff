7560041b95af40d0ef0dce008f18f6536052da0d
Pbra1484 <pbra1484@740s-pl-108.local>
2016-11-01 12:51:09 -0600

    added buttons

--- src/gui/view/FirstPanel.java
+++ src/gui/view/FirstPanel.java
@@ -2,10 +2,14 @@ package gui.view;
 
 import javax.swing.JPanel;
 import gui.controller.GUIController;
+import java.awt.Color;
+import javax.swing.JButton;
 
 public class FirstPanel extends JPanel
 {
 	private GUIController baseController;
+	private JButton colorButton;
+	private JButton randomButton;
 	
 	
 	public FirstPanel(GUIController baseController)
@@ -13,14 +17,21 @@ public class FirstPanel extends JPanel
 		super();
 		this.baseController = baseController;
 		
+		this.colorButton = new JButton("colors");
+		this.randomButton = new JButton("random colors");
+		
 		setupPanel();
 		setupLayout();
 		setupListeners();
+
 		
 	}
 	
 	private void setupPanel()
 	{
+		this.setBackground(Color.BLACK);
+		this.add(colorButton);
+		this.add(randomButton);
 		
 	}
 	
